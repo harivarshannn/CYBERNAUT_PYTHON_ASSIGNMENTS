3608d81dc2cc82d9c13833a5a45ebf9352a4f061
harivarshannn <harivarshan2006@outlook.com>
2024-10-06 17:00:15 +0530

    added floataddition.py

---
+++ DAY2/floataddition.py
@@ -0,0 +1,14 @@
+
+x = int(input("Enter integer x: "))
+y = int(input("Enter integer y: "))
+
+result=x+y
+print(f"The result of adding  as integer {x} and {y} is: {result}")
+
+x_float = float(x)
+y_float = float(y)
+
+
+result = x_float + y_float
+
+print(f"The result of adding  as float {x_float} and {y_float} is: {result}")
