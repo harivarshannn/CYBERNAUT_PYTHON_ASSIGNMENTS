4488b4331547dec4605b80a4775d04a0dace6ecf
harivarshannn <harivarshan2006@outlook.com>
2024-10-27 13:53:54 +0530

    added namevalue.py

---
+++ PRACTICAL/namevalue.py
@@ -0,0 +1,8 @@
+import random
+def assign_random_value(name):
+    random_value = random.randint(1, 50)
+    print(f"The value assigned to {name} is {random_value}")
+
+
+user_name = input("Enter your name: ")
+assign_random_value(user_name)
