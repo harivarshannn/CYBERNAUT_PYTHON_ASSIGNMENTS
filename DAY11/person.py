a257792cb6a354744077bc2590124b6182b918b5
harivarshannn <harivarshan2006@outlook.com>
2024-10-27 19:42:51 +0530

    added person.py

---
+++ DAY11/person.py
@@ -0,0 +1,10 @@
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+per = person("Harivarshan", 18)
+per.display_info()
